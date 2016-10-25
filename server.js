5be4e65fed5f3a1fef64dde91ff3591aaf6d292f
Thali Ram <Thali Ram>
2016-10-25 08:25:29 -0500

    server de texto con configurador modularizado

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 var http = require ('http');
+var colors = require ('colors')
+
+//Cargando configuraciones
+var config = require('./config/config.js')
+var IP = 'config.IP';
+var PORT = 'config.PORT';
+
+//Tema de colors
+colors.setTheme (config.color_theme);
 var server = http.createServer (function(req,res){
     //codigo de nuestro server
     res.writeHead (200,{
@@ -6,6 +15,6 @@ var server = http.createServer (function(req,res){
     });
     res.end ('Server funcionando');
 });
-server.listen (3000,'127.0.0.1', function(){
-    console.log('Server working @http://127.0.0.1:3000/');
+server.listen (PORT, IP, function(){
+    console.log('Server working @http://$(IP):$(PORT)/');
 });
